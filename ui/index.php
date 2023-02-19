3e2a6e5360aa85ef4ed05f421f6a28c2de0d57df
iamrjb11 <iamrjb11@gmail..com>
2023-02-19 11:51:17 +0600

    first commit

---
+++ ui/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home Page</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+	<div class=""></div>
+	<p></p>
+	<span></span>
+	<input type="text" name="" placeholder="Enter your name">
+	<input type="button" name="" value="button-rjb">
+	<input type="submit" name="" value="submit-rjb" class="btn btn-primary">
+	<br>
+	<h1>ytytr</h1>
+
+	
+	
+</body>
+</html>
+
+
